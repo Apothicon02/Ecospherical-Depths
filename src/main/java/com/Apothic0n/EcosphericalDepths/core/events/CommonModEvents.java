807998ed886b5579a8e74b6149cd86c5d1cf96fd
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2022-08-29 10:51:47 -0400

    - Updated mod version to 0.2.2. - Fixed not being able to enter the dimension.

--- src/main/java/com/Apothic0n/EcosphericalDepths/core/events/CommonModEvents.java
+++ src/main/java/com/Apothic0n/EcosphericalDepths/core/events/CommonModEvents.java
@@ -2,6 +2,9 @@ package com.Apothic0n.EcosphericalDepths.core.events;
 
 import com.Apothic0n.EcosphericalDepths.EcosphericalDepths;
 import com.Apothic0n.EcosphericalDepths.core.objects.EcodBlocks;
+import com.Apothic0n.EcosphericalDepths.world.dimension.DepthsDimensions;
+import com.Apothic0n.EcosphericalDepths.world.dimension.DepthsITeleporter;
+import com.Apothic0n.EcosphericalDepths.world.dimension.DepthsTeleporter;
 import com.cursedcauldron.wildbackport.common.registry.worldgen.WBFeatures;
 import com.cursedcauldron.wildbackport.common.registry.worldgen.WBWorldGeneration;
 import com.github.alexthe666.alexsmobs.AlexsMobs;
@@ -11,12 +14,17 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
@@ -34,6 +42,9 @@ import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityTeleportEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -43,10 +54,81 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.orcinus.galosphere.init.GConfiguredFeatures;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 
 
 @Mod.EventBusSubscriber(modid = EcosphericalDepths.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CommonModEvents {
+
+    @SubscribeEvent
+    public static void chorusTeleport(EntityTeleportEvent.ChorusFruit event) {
+        LivingEntity Player = event.getEntityLiving();
+        if (!Player.level.isClientSide()) {
+            MinecraftServer server = Player.getServer();
+            if (server != null) {
+                ServerLevel overWorld = server.getLevel(Level.OVERWORLD);
+                ServerLevel depthsDim = server.getLevel(DepthsDimensions.DepthsDim);
+                BlockState standingOn = Player.level.getBlockState(Player.blockPosition().below());
+                BlockState standingUnder = Player.level.getBlockState(Player.blockPosition().above(2));
+                if (standingUnder != null || standingOn != null) {
+                    BlockState bedrock = Blocks.BEDROCK.defaultBlockState();
+                    if (Player.level.dimension() == Level.OVERWORLD && standingOn == bedrock) {
+                        Player.changeDimension(depthsDim, new DepthsTeleporter(Player.blockPosition(), false));
+                    } else if (Player.level.dimension() == DepthsDimensions.DepthsDim && standingUnder == bedrock) {
+                        Player.changeDimension(overWorld, new DepthsTeleporter(Player.blockPosition(), true));
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void playerTick(TickEvent.PlayerTickEvent event) {
+        Player player = event.player;
+        MinecraftServer minecraftServer = player.getServer();
+        Level level = player.level;
+        if (level.isClientSide) { //Do something to the player from the client every tick
+            if (!player.getCooldowns().isOnCooldown(Items.AIR) && !ModList.get().isLoaded("inversia")) { //Use a shared 50s delay if Inversia isn't installed. DO NOT USE WITH TWO THINGS THAT CAN HAPPEN AT THE SAME TIME
+                if (level.getBlockState(player.blockPosition().below()) == Blocks.BEDROCK.defaultBlockState() && level.dimension().equals(Level.OVERWORLD)) {
+                    player.displayClientMessage(new TranslatableComponent("block.minecraft.bedrock.under"), true);
+                    player.playSound(SoundEvents.PORTAL_AMBIENT, 0.3f, 0.8f);
+                    player.getCooldowns().addCooldown(Items.AIR, 1000);
+                } else if (level.getBlockState(player.blockPosition().above(2)) == Blocks.BEDROCK.defaultBlockState() && level.dimension().equals(DepthsDimensions.DepthsDim)) {
+                    player.displayClientMessage(new TranslatableComponent("block.minecraft.bedrock.above"), true);
+                    player.playSound(SoundEvents.PORTAL_AMBIENT, 0.3f, 0.8f);
+                    player.getCooldowns().addCooldown(Items.AIR, 1000);
+                }
+            }
+        } else if (minecraftServer != null) { //Do something to the player from the server every tick
+            ServerPlayer serverPlayer = ((ServerPlayer) event.player);
+            if (Level.NETHER.equals(level.dimension()) && !ModList.get().isLoaded("inversia")) { //Do something to the player from the server as long as they are in the nether dimension and inversia is not installed
+                if (player.blockPosition().getY() >= 320) {
+                    ArrayList teleporterData = DepthsITeleporter.acsendFromNetherRoof(serverPlayer);
+                    ServerPlayer newServerPlayer = (ServerPlayer) teleporterData.get(0);
+                    BlockPos newPlayerPosition = (BlockPos) teleporterData.get(1);
+                    newServerPlayer.teleportTo(minecraftServer.getLevel(DepthsDimensions.DepthsDim), newPlayerPosition.getX(), newPlayerPosition.getY(), newPlayerPosition.getZ(), 0, 0);
+                }
+            } else if (DepthsDimensions.DepthsDim.equals(level.dimension())) { //Do something to the player from the server as long as they are in the depths dimension
+                if (player.blockPosition().getY() <= -144) {
+                    ArrayList teleporterData = DepthsITeleporter.fallToNetherRoof(serverPlayer);
+                    ServerPlayer newServerPlayer = (ServerPlayer) teleporterData.get(0);
+                    BlockPos newPlayerPosition = (BlockPos) teleporterData.get(1);
+                    newServerPlayer.teleportTo(minecraftServer.getLevel(Level.NETHER), newPlayerPosition.getX(), newPlayerPosition.getY(), newPlayerPosition.getZ(), 0, 0);
+                    player.getCooldowns().addCooldown(Items.INK_SAC, 200);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void playerFall(LivingFallEvent event) {
+        LivingEntity livingEntity = (LivingEntity) event.getEntity();
+        if (livingEntity instanceof ServerPlayer serverPlayer && !ModList.get().isLoaded("inversia")) {
+            if (serverPlayer.getCooldowns().isOnCooldown(Items.INK_SAC)) {
+                event.setCanceled(true);
+            }
+        }
+    }
 
     @SubscribeEvent
     public static void itemUsed(PlayerInteractEvent.RightClickBlock event) {
